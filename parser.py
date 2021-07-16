239d68d23582e0f5e0c688e0ce0780ba408c32ac
kaidi <kaidi.chen@rokid.com>
2021-07-16 02:59:20 +0000

    upload parse-anno sample

---
+++ parser.py
@@ -0,0 +1,38 @@
+import json
+
+
+def parse(path):
+    with open(path, 'r') as f:
+        content = json.load(f)
+
+    '''
+        ['attribute', 'config', 'file', 'metadata', 'project', 'view']
+    '''
+    fname_dict = {}
+    for vid in content['file'].keys():
+        fname = content['file'][vid]['fname']
+        fname_dict[fname] = vid
+
+    obj_dict = {}
+
+    '''
+        {
+            'av': {'1': '0'},    # category info
+            'flg': 0, 
+            'vid': '1',  
+            'xy': [2, 208.171, 66.488, 386.265, 321.359], 
+            'z': []
+        }
+    '''
+    
+    for obj_id in content['metadata'].keys():
+        vid = content['metadata'][obj_id]['vid']
+        pts = content['metadata'][obj_id]['xy']
+        if obj_dict.get(vid) is None:
+            obj_dict[vid] = [pts]
+        else:
+            obj_dict[vid].append(pts)
+
+
+if __name__ == "__main__":
+    parse('./sample/label/via.json')
